bb8f34efee70579626d63c0b16493eaed15d7f7b
Javier <Jvasquez5422@pelhamprepacademy.org>
2023-07-31 15:52:59 -0400

    Finished

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Add your code to this file
+
+let body = document.querySelector("body");
+body.style["background-color"] = "blue";
+
+let images = document.querySelectorAll("img");
+
+for(let f = 0; f < images.length; f++){
+
+    images[f].style["border-style"] = "dashed";
+    images[f].style["border-color"] = "orange";
+
+}
+
+console.log(images);
+
